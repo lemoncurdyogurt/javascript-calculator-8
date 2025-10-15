e14d8e3013818fffbc568043a652463df6d940a0
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-15 16:48:36 +0900

    feat(app): 기본 구분자에 대한 입력값 split 처리

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      await this.getSumNumbers();
+      const input = await this.getSumNumbers();
+      const result = this.splitAndSum(input);
+      Console.print(`결과 : ${result}`);
     } catch (error) {
       //에러처리
       Console.print(`[ERROR] ${error.message}`);
@@ -13,9 +15,17 @@ class App {
       await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
     if (!input || input.trim() === '') {
       Console.print('결과 : 0');
-      return;
+      return null;
     }
+    return input;
+  }
+  splitAndSum(input) {
+    if (!input) return 0;
+    const separators = [',', ':'];
+    const regex = new RegExp(separators.join('|'));
+    const numbers = input.split(regex).map(num => Number(num));
+    const sum = numbers.reduce((acc, number) => acc + number, 0);
+    return sum;
   }
 }
-
 export default App;
