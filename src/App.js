14d887fb467ee36de9a52b0052d284dd1cc9adfb
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-15 19:09:25 +0900

    feat: 커스텀구분자에서 빈문자열인 경우 에러처리 코드 추가

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ class App {
       const result = this.splitAndSum(input);
       Console.print(`결과 : ${result}`);
     } catch (error) {
-      //에러처리
       Console.print(`[ERROR] ${error.message}`);
     }
   }
   async getSumNumbers() {
     const input =
       await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+
     if (!input || input.trim() === '') {
       Console.print('결과 : 0');
       return null;
@@ -22,29 +22,42 @@ class App {
 
   splitAndSum(input) {
     if (!input) return 0;
+
     const { customSeparator, numbersPart } = this.customSeparator(input);
     const separators = [',', ':'];
 
     let target = input;
     if (customSeparator) {
-      separators.push(customSeparator);
+      separators.push(this.escapeRegExp(customSeparator));
       target = numbersPart;
     }
 
-    const regex = new RegExp(separators.join('|'));
-    const numbers = input.split(regex).map(num => Number(num));
-    const sum = numbers.reduce((acc, number) => acc + number, 0);
+    const regex = new RegExp(separators.join('|'), 'g');
+    const numbers = target.split(regex).map(num => Number(num));
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
     return sum;
   }
-  
+
   customSeparator(input) {
     if (input.startsWith('//')) {
       const separatorEndIndex = input.indexOf('\n');
-      const customSeparator = input.substring(2, separatorEndIndex);
+      // \n이 없는 경우 에러 처리
+      if (separatorEndIndex === -1) throw new Error('커스텀 구분자 형식 오류');
+
+      const potentialSeperator = input.substring(2, separatorEndIndex);
+      // 빈문자열인 경우 에러 처리
+      if (potentialSeperator === "") throw new Error('커스텀 구분자 형식 오류');
+      const customSeparator = potentialSeperator;
+
       const numbersPart = input.substring(separatorEndIndex + 1);
       return { customSeparator, numbersPart };
     }
     return { customSeparator: null, numbersPart: input };
+  }
+
+  //정규식 특수문자 이스케이프 처리
+  escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   }
 }
 export default App;
