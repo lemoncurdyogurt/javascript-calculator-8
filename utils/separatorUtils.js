c79b41e589ccdaa9a5ee13d4acca225afd0d68b3
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-20 01:14:16 +0900

    refactor(separatorUtils): 커스텀구분자 유틸 분리

---
+++ utils/separatorUtils.js
@@ -0,0 +1,16 @@
+export function customSeparator(input) {
+  if (!input.startsWith('//')) {
+    return { customSeparator: null, numbersPart: input };
+  }
+
+  const separatorEndIndex = input.indexOf('\\n');
+  if (separatorEndIndex === -1)
+    throw new Error('[ERROR] 커스텀 구분자 형식 오류');
+
+  const customSeparator = input.substring(2, separatorEndIndex);
+  if (customSeparator === '')
+    throw new Error('[ERROR] 커스텀 구분자 형식 오류');
+
+  const numbersPart = input.substring(separatorEndIndex + 2);
+  return { customSeparator, numbersPart };
+}
